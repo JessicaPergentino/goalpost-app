1a022072ad20242ce7904c47da252e723335a691
Jessica Pergentino <jessica.pergentino@capgemini.com>
2025-05-05 10:47:31 -0300

    Criando o controlador de visualização da meta de criação

---
+++ goalpost-app/Enums/GoalType.swift
@@ -0,0 +1,13 @@
+//
+//  GoalType.swift
+//  goalpost-app
+//
+//  Created by Capgemini on 02/05/25.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
